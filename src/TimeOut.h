5786ca15e67e9d82c82c30583d56f98b93c6cdec
NitrofMtl <o_for@hotmail.com>
2021-01-05 05:32:39 -0500

    consolidate paramPack

--- src/TimeOut.h
+++ src/TimeOut.h
@@ -71,9 +71,10 @@ protected:
 
 template <typename ... Args>
 class TimeOutNodeArgs : public TimeOutNode {
-	TimeOutNodeArgs<Args...>(Args ... ts, void (*cb)(Args ..._args) ) : args(ts...), pack(ParamsPack<Args...> { args, cb } ){};
+	//TimeOutNodeArgs<Args...>(Args ... ts, void (*cb)(Args ..._args) ) : args(ts...), pack(ParamsPack<Args...> { args, cb } ){};
+	TimeOutNodeArgs<Args...>(Args ... arg, void (*cb)(Args ...) ) : pack(ParamsPack<Args...> { MicroTuple<Args...>(arg...), cb } ){};
 	friend class TimeOut;
-	MicroTuple<Args...> args;
+	//MicroTuple<Args...> args;
 	ParamsPack<Args...> pack;
 	void callbackTrigger() { if(pack) pack.getPack();};
 };
